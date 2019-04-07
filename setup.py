4fdb3435ec8a2f5b64e9127915fac7dc7ee0dfa1
Ryan Han <ryan.han@uwaterloo.ca>
2019-04-07 15:44:26 -0400

    update tests

--- setup.py
+++ setup.py
@@ -23,4 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    test_suite='nose.collector',
+    tests_require=['nose']
 )
